c7cc90dc13bc2eba67150e68c617dae760e5efc4
unknown <bolarinwaahmed22@gmail.com>
2021-09-09 01:29:03 +0100

    Creating a simple Reaction Timer in Vue

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1>Reaction Timer</h1>
+  <button @click="start" :disabled="isPlaying">Play</button>
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <!-- <h3 v-if="showResults">Reaction time: {{ score }} ms</h3> -->
+  <Result :result="score" v-if="showResults" />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Block from "./components/Blocks.vue";
+import Result from "./components/Result.vue";
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { Block, Result },
+  data() {
+    return {
+      isPlaying: false,
+      delay: null,
+      score: null,
+      showResults: false,
+    };
+  },
+  methods: {
+    start() {
+      this.delay = 2000 + Math.random() * 5000; // Generating a random number between 2000 and 7000 miliseconds
+      this.isPlaying = true;
+      this.showResults = false;
+    },
+
+    endGame(reactionTime) {
+      this.score = reactionTime;
+      this.isPlaying = false;
+      this.showResults = true;
+    },
+  },
+};
 </script>
 
 <style>
@@ -20,7 +42,24 @@ export default {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: #2c3e50;
+  color: #444;
   margin-top: 60px;
+}
+
+button {
+  color: #fff;
+  background: #0faf87;
+  padding: 8px 16px;
+  border: none;
+  border-radius: 4px;
+  font-size: 16px;
+  letter-spacing: 1px;
+  cursor: pointer;
+  margin: 10px;
+}
+
+button[disabled] {
+  opacity: 0.2;
+  cursor: not-allowed;
 }
 </style>
---
+++ src/components/Result.vue
@@ -0,0 +1,33 @@
+<template>
+  <h4>Reaction time - {{ result }} ms</h4>
+  <p class="rank">{{ rank }}</p>
+</template>
+
+<script>
+export default {
+  props: ["result"],
+  data() {
+    return {
+      rank: null,
+    };
+  },
+
+  mounted() {
+    if (this.result < 250) {
+      this.rank = "Ninja Fingers";
+    } else if (this.result < 400) {
+      this.rank = "Rapid Reflexes";
+    } else {
+      this.rank = "Snail Pace";
+    }
+  },
+};
+</script>
+
+<style>
+.rank {
+  font-size: 1.4rem;
+  color: #0faf87;
+  font-weight: bold;
+}
+</style>
